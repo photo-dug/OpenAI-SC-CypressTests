5785a13d1f4ecfc6abb40956ff5c52fbcb4f6415
Doug Ross <124635749+photo-dug@users.noreply.github.com>
2025-09-04 11:50:08 -0700

    Update cypress.config.cjs
    missing a comma DOH!

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -23,7 +23,7 @@ module.exports = defineConfig({
     FINGERPRINT_STRICT: process.env.FINGERPRINT_STRICT === 'true',
     // accept both SKIP_AUDIO and CYPRESS_SKIP_AUDIO
     SKIP_AUDIO:(process.env.CYPRESS_SKIP_AUDIO ?? process.env.SKIP_AUDIO ?? 'false'),
-}
+},
 
   }),
   e2e: {
