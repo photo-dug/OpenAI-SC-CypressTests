58f072ba7ebb83abcda63acbc4761c3a3c8e852d
Doug Ross <124635749+photo-dug@users.noreply.github.com>
2025-10-29 16:18:41 -0700

    Update soundcredit.e2e.cy.js

--- cypress/e2e/soundcredit.e2e.cy.js
+++ cypress/e2e/soundcredit.e2e.cy.js
@@ -533,4 +533,3 @@ it('06 – Click track #1 to start playback', () => {
       // (optional) also drop a small pointer file so you can click/open it easily
       cy.writeFile('cypress/reports/_results_pointer.txt', String(outPath) + '\n', { log: false });
   });
-});
